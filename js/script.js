60498cc2f675633f0094cb6a78151bb14ce988ca
Isabela Bastos <mb.isabela@gmail.com>
2017-05-15 16:07:20 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function startUp(){
+    document.getElementById('blackOut').style.display = 'none';
+    document.getElementById('contactForm').style.display = 'none';
+}
+
+function showForm() {
+    document.getElementById('blackOut').style.display = 'block';
+    document.getElementById('contactForm').style.display = 'block';
+
+}
+
+function closeBox() {
+    document.getElementById('blackOut').style.display = 'none';
+    document.getElementById('contactForm').style.display = 'none';
+}
